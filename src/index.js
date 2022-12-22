867a1ea26cbe95b56588823aab3f96c1ad60bde0
unknown <summerkate.nadela@gmail.com>
2022-12-23 00:16:59 +0800

    eleventh commit

--- src/index.js
+++ src/index.js
@@ -12,14 +12,13 @@ import PetPage from "./pages/PetPage";
 
 import { HashRouter, Route } from "react-router-dom";
 
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
 	<React.StrictMode>
 		<HashRouter>
 			<App />
 		</HashRouter>
-	</React.StrictMode>,
-	document.getElementById("root")
+	</React.StrictMode>
 );
 
 // root.render(
