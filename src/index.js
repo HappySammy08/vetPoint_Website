0aa88e976293bc934791702815e71a04f2eb498e
unknown <summerkate.nadela@gmail.com>
2022-12-23 00:36:19 +0800

    thirteen commit

--- src/index.js
+++ src/index.js
@@ -15,9 +15,7 @@ import { HashRouter, Route } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<React.StrictMode>
-		<HashRouter>
-			<App />
-		</HashRouter>
+		<App />
 	</React.StrictMode>
 );
 
