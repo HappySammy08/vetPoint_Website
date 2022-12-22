678a71644ffdb623f3d808e733fd431af2091c6b
unknown <summerkate.nadela@gmail.com>
2022-12-22 23:43:12 +0800

    fifth commit

--- src/index.js
+++ src/index.js
@@ -12,6 +12,13 @@ import PetPage from "./pages/PetPage";
 
 import { BrowserRouter, Route } from "react-router-dom";
 
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
+);
+
 // const router = createBrowserRouter([
 // 	{
 // 		path: "/",
@@ -37,13 +44,15 @@ import { BrowserRouter, Route } from "react-router-dom";
 // const root = ReactDOM.createRoot(document.getElementById("root"));
 // root.render(<RouterProvider router={router} />);
 
-<BrowserRouter basename={process.env.PUBLIC_URL}>
+{
+	/* <BrowserRouter basename={process.env.PUBLIC_URL}>
 	<Route path="/" component={App} />
 	<Route path="/login" component={LoginPage} />
 	<Route path="/appointmentForm" component={AppointmentForm} />
 	<Route path="/profilePage" component={ProfilePage} />
 	<Route path="/petpage" component={PetPage} />
-</BrowserRouter>;
+</BrowserRouter>; */
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
