b7bb1dd20ababfdaf149fe89fb1cd4c532c18126
unknown <summerkate.nadela@gmail.com>
2022-12-23 12:03:14 +0800

    24

--- src/index.js
+++ src/index.js
@@ -7,13 +7,13 @@ import "./index.css";
 import App from "./App";
 import { HashRouter } from "react-router-dom";
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
 	<React.StrictMode>
 		<HashRouter>
 			<App />
 		</HashRouter>
 	</React.StrictMode>,
-
 	document.getElementById("root")
 );
 // const root = ReactDOM.createRoot(document.getElementById("root"));
